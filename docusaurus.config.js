8e365176352acceaa0ff64fbff340dace23aeaee
Joao Sousa <a79300@ualg.pt>
2023-11-24 10:45:31 +0000

    Alteracao do ficheiro docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Test Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Oficina Temas Turbo',
+  tagline: 'Ganda Bote',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
